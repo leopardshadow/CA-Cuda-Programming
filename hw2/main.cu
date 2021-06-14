3e509eefc7a45b412afd12351d74550387d5e102
leopardshadow <thurtu811@gmail.com>
2021-06-14 20:16:22 +0800

    add simulations using GPU ((first version

--- hw2/main.cu
+++ hw2/main.cu
@@ -2,10 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "parameters.h"
+
 #define T(x, y) ((x) + (N+2)*(y))
 
-const int N = 5; // NxN grid
-int M = 1; // M rounds
+extern float GPU_kernel(int *B, int *A);
 
 u_int8_t state[N][N] = {
     {'1','0','0','0','1'},
@@ -17,51 +18,102 @@ u_int8_t state[N][N] = {
 
 u_int8_t event[2][ (N+2) * (N+2) ] =  {0};
 
-int simulate(int r) {
+u_int8_t *gpuEvent[2];
+
+u_int8_t *result;
+
+void simulate(u_int8_t *now, u_int8_t *next) {
     int i, j, t;
-    int nr = (r + 1) % 2;
     int adjac;
     for(i = 1; i <= N; i++) {
         for(j = 1; j <= N; j++) {
             adjac = 
-                event[r][T(i-1, j-1)] + event[r][T(i, j-1)] + event[r][T(i+1, j-1)] + 
-                event[r][T(i-1, j)]   +                     + event[r][T(i+1, j)]   + 
-                event[r][T(i-1, j+1)] + event[r][T(i, j+1)]   + event[r][T(i+1, j+1)];
+                now[T(i-1, j-1)] + now[T(i, j-1)] + now[T(i+1, j-1)] + 
+                now[T(i-1, j)]   +                + now[T(i+1, j)]   + 
+                now[T(i-1, j+1)] + now[T(i, j+1)] + now[T(i+1, j+1)];
 
 
-            t = event[r][T(i, j)];
+            t = now[T(i, j)];
 
     
             if((!t && adjac == 3) || (t && adjac == 2) || (t && adjac == 3)) {
-                event[nr][T(i, j)] = 1;
+                next[T(i, j)] = 1;
             }
             else {
-                event[nr][T(i, j)] = 0;
+                next[T(i, j)] = 0;
             }
-            // printf("%d %d: %c - %d\n", i, j, '0'+event[nr][T(i, j)], adjac);
+            // printf("%d %d: %c - %d\n", i, j, '0'+next[T(i, j)], adjac);
         }
 
     }
-    return nr;
 }
 
 
-int runSimulations() {
+void runSimulations() {
     
-    int r = 0;
-
     // do simulation m times
     for(int m = 0; m < M; m++) {
 
-        r = simulate(r);
+        simulate(event[m%2], event[(m+1)%2]);
     }
-    return r;
+    result = event[M%2];
 }
+
+__global__ void gpu_simulate(u_int8_t *dNow, u_int8_t *dNext)
+{	
+    int i = threadIdx.x + 1;
+    int j = blockIdx.x + 1;
+
+
+    int t, adjac;
+    
+    adjac = 
+        dNow[T(i-1, j-1)] + dNow[T(i, j-1)] + dNow[T(i+1, j-1)] + 
+        dNow[T(i-1, j)]   +                 + dNow[T(i+1, j)]   + 
+        dNow[T(i-1, j+1)] + dNow[T(i, j+1)] + dNow[T(i+1, j+1)];
+
+
+    t = dNow[T(i, j)];
+
+
+    if((!t && adjac == 3) || (t && adjac == 2) || (t && adjac == 3)) {
+        dNext[T(i, j)] = 1;
+    }
+    else {
+        dNext[T(i, j)] = 0;
+    }
+}
+
+
+
+void runGPUSimulations() {
+    
+
+	cudaMalloc((void**)&gpuEvent[0], sizeof(u_int8_t)*((N+2)*(N+2)));
+	cudaMalloc((void**)&gpuEvent[1], sizeof(u_int8_t)*((N+2)*(N+2)));
+
+    cudaMemcpy(gpuEvent[0], event[0], sizeof(u_int8_t)*((N+2)*(N+2)), cudaMemcpyHostToDevice);
+    cudaMemcpy(gpuEvent[1], event[1], sizeof(u_int8_t)*((N+2)*(N+2)), cudaMemcpyHostToDevice);
+
+    // Launch Kernel
+	dim3 dimGrid(blockNum);
+    dim3 dimBlock(gridNum);
+
+    for(int m = 0; m < M; m++) {
+        gpu_simulate<<<dimGrid,dimBlock>>>(gpuEvent[m%2], gpuEvent[(m+1)%2]);
+    }
+
+	cudaMemcpy(event[0], gpuEvent[0], sizeof(u_int8_t)*((N+2)*(N+2)), cudaMemcpyDeviceToHost);
+	cudaMemcpy(event[1], gpuEvent[1], sizeof(u_int8_t)*((N+2)*(N+2)), cudaMemcpyDeviceToHost);
+
+    result = event[M%2];
+}
+
+
 
 int main(int argc, char const *argv[])
 {
     int i, j;
-    int r;
 
     for(i = 1; i <= N; i++) {
         for(j = 1; j <= N; j++) {
@@ -78,14 +130,25 @@ int main(int argc, char const *argv[])
     printf("\n-----\n");
 
     
-    r = runSimulations();
+    // runSimulations();
+
+    // for(i = 0; i <= N+1; i++) {
+    //     for(j = 0; j <= N+1; j++) {
+    //         printf("%c", result[T(i, j)] + '0');
+    //     }
+    //     printf("\n");
+    // }
+
+
+    runGPUSimulations();
 
     for(i = 0; i <= N+1; i++) {
         for(j = 0; j <= N+1; j++) {
-            printf("%c", event[r][T(i, j)] + '0');
+            printf("%c", result[T(i, j)] + '0');
         }
         printf("\n");
     }
+
 
 
     return 0;
---
+++ hw2/parameters.h
@@ -0,0 +1,5 @@
+const int N = 5; // NxN grid
+int M = 1; // M rounds
+
+const int gridNum = 5;
+const int blockNum = 5;
